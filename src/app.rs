9843f12866d7e6e517f2314bfc0152d80ecf7bd4
saent-x <torsxperience@outlook.com>
2025-05-15 12:13:24 +0100

    added new properties to app

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use crate::models::MusicGenre;
+use crate::models::{MusicEra, MusicGenre};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -6,6 +6,7 @@ use crate::models::MusicGenre;
 pub struct App {
     username: String,
     music_genre: MusicGenre,
+    music_era: MusicEra
 }
 
 impl Default for App {
@@ -13,6 +14,7 @@ impl Default for App {
         Self {
             username: "default-user".to_string(),
             music_genre: MusicGenre::Any,
+            music_era: MusicEra::Any
         }
     }
 }
@@ -44,67 +46,12 @@ impl eframe::App for App {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
-
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.label("Simple EGUI label");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
-                powered_by_egui_and_eframe(ui);
-                simple_element(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            simple_element(ui);
         });
     }
-}
-
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
 }
 
 fn simple_element(ui: &mut egui::Ui) {
